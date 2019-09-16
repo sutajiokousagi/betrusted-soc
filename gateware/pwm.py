bbb852e2a2ce8cb33f1d2c00dfc845ded03d414c
bunnie <bunnie@kosagi.com>
2019-09-16 18:51:17 +0800

    add gateware directory

---
+++ gateware/pwm.py
@@ -0,0 +1,36 @@
+from migen import *
+
+from litex.soc.interconnect.csr import *
+
+
+class PWM(Module, AutoCSR):
+    def __init__(self, pwm):
+        self._enable = CSRStorage()
+        self._width = CSRStorage(32)
+        self._period = CSRStorage(32)
+        self.hardware_ena = Signal()
+
+        # # #
+
+        cnt = Signal(32)
+
+        enable = self._enable.storage | self.hardware_ena
+        width = self._width.storage
+        period = self._period.storage
+
+        self.sync += \
+            If(enable,
+                If(cnt < width,
+                    pwm.eq(1)
+                ).Else(
+                    pwm.eq(0)
+                ),
+                If(cnt == period-1,
+                    cnt.eq(0)
+                ).Else(
+                    cnt.eq(cnt+1)
+                )
+            ).Else(
+                cnt.eq(0),
+                pwm.eq(0)
+            )
