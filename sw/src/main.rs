a8b8fe69be22af1d845a57547f78366b3d22775b
bunnie <bunnie@kosagi.com>
2019-11-11 16:18:09 +0800

    add an initial test main.rs

---
+++ sw/src/main.rs
@@ -0,0 +1,39 @@
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+use riscv_rt::entry;
+
+extern crate betrusted_hal;
+
+const CONFIG_CLOCK_FREQUENCY: u32 = 100_000_000;
+
+// allocate a global, unsafe static string for debug output
+#[used] // This is necessary to keep DBGSTR from being optimized out
+static mut DBGSTR: [u32; 4] = [0, 0, 0, 0];
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
+
+#[entry]
+fn main() -> ! {
+    use betrusted_hal::hal_i2c::hal_i2c::*;
+    use betrusted_hal::hal_time::hal_time::*;
+
+    let p = betrusted_pac::Peripherals::take().unwrap();
+
+//    i2c_init(&p, CONFIG_CLOCK_FREQUENCY / 1_000_000);
+    time_init(&p);
+
+    // flash an LED!
+    loop {
+        delay_ms(&p, 500);
+        unsafe{ DBGSTR[0] = 4; }
+
+        delay_ms(&p, 500);
+        unsafe{ DBGSTR[0] = 8; }
+
+    }
+}
